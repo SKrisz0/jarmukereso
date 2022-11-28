1a20dba4755a872f7223c00063b0d23a73a639fd
SKrisz0 <93580605+SKrisz0@users.noreply.github.com>
2022-11-28 22:26:23 +0100

    utolso

--- data.py
+++ data.py
@@ -1,1 +1,61 @@
-cars = {}
+szuperSport=[
+    {'marka': 'Bugatti Chiron', 'HP': '1578', 'price': '1 240 656 860ft'},
+    {'marka': 'Ferrari LaFerrari', 'HP': '950', 'price': '960 456 000ft'},
+]
+muscle=[
+    {'marka': 'Ford Mustang GT-500', 'HP': '490', 'price': '12 340 000ft'},
+    {'marka': 'Dodge Challenger Demon', 'HP': '840', 'price': '26 999 999ft'},
+    {'marka': 'Dodge Charger R/T', 'HP': '370', 'price': '17 876 940ft'},
+]
+sport=[
+    {'marka': 'Nissan GT-R R35', 'HP': '536', 'price': '29 900 000ft'},
+    {'marka': 'Nissan GT-R R34', 'HP': '231', 'price': '24 500 000ft'},
+    {'marka': 'Toyota Supra Mk4', 'HP': '321', 'price': '28 999 000ft'},
+    {'marka': 'Nissan Silvia S15', 'HP': '250', 'price': '19 860 000ft'},
+    {'marka': 'Toyota Celica GT', 'HP': '180', 'price': '4 500 000ft'},
+]
+hetszem=[
+    {'marka': 'Fiat 500-L Living 56', 'HP': '95', 'price': '1 200 000ft'},
+    {'marka': 'Peugeot Rifter 51', 'HP': '87', 'price': '980 000ft'},
+]
+nyolcszem=[
+    {'marka': 'Hyundai H-1 Starex', 'HP': '444', 'price': '3 430 000ft'},
+    {'marka': 'Opel Vivaro', 'HP': '134', 'price': '780 000ft'},
+]
+kilencszem=[
+    {'marka': 'Renault Tourneo', 'HP': '140', 'price': '1 020 100ft'},
+]
+nehezterep=[
+    {'marka': 'Ford Ranger', 'HP': '490', 'price': '13 640 000ft'},
+    {'marka': 'Dodge RAM 1500', 'HP': '809', 'price': '7 560 000ft'},
+    {'marka': 'Ford Raptor', 'HP': '670', 'price': '4 630 000ft'},
+]
+varossuv=[
+    {'marka': 'Toyota RAV4', 'HP': '190', 'price': '7 500 000ft'},
+    {'marka': 'BMW X6', 'HP': '460', 'price': '13 460 000ft'},
+    {'marka': 'Hyundai Tucson', 'HP': '170', 'price': '4 800 000ft'},
+]
+legszurok=[
+    {'marka': 'LG-MT2504C', 'HP': '-', 'price': '4000ft'},
+    {'marka': 'LG-MT2504R', 'HP': '-', 'price': '4500ft'},
+]
+turbok=[
+    {'marka': 'WRX STI', 'HP': '-', 'price': '560 000ft'},
+    {'marka': 'GTR TRB', 'HP': '-', 'price': '1 200 000ft'},
+    {'marka': 'CLC GT', 'HP': '-', 'price': '460 870ft'},
+]
+ledek=[
+    {'marka': 'Kiürült a készlet', 'HP': '-', 'price': '-'},
+]
+spoiler=[
+    {'marka': '993 Ca Légbeömlő', 'HP': '-', 'price': '21 000ft'},
+    {'marka': 'FxSK03 Lökhárító toldat', 'HP': '-', 'price': '35 000ft'},
+    {'marka': 'PP-LT-060 Uiverzális Karbon szárny', 'HP': '-', 'price': '169 000ft'},
+    {'marka': 'CA13 küszöb spoiler', 'HP': '-', 'price': '24 000ft'},
+]
+felnik=[
+    {'marka': 'Jr14 16"X8" Festett fekete-bronz alufelni', 'HP': '-', 'price': '86 000ft/db'},
+    {'marka': 'Jr11 18"X10" Fényes fekete alufelni', 'HP': '-', 'price': '125 000ft/db'},
+    {'marka': 'Jr10 15"X7" Ezüst alufelni', 'HP': '-', 'price': '70 000ft/db'},
+    {'marka': 'Jr20 19"X4" karbon bevonatú ezüst felni', 'HP': '-', 'price': '1 100 000ft/4 db'},
+]
