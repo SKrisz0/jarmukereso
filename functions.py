b20ceae5eca0bb92e36fdd96a8ab77a2ad7b63be
SKrisz0 <93580605+SKrisz0@users.noreply.github.com>
2022-11-28 22:23:50 +0100

    vegso

--- functions.py
+++ functions.py
@@ -1,25 +1,93 @@
 from data import *
 from os import system
 
-filename = 'data.csv'
+filename = 'data.py'
 cimsor = ''
 
 def menu():
     system('cls')
     print('Milyen jellegű autót keres?')
-    print('0 - Mindegy')
-    print('1 - Sport')
-    print('2 - Couple')
-    print('3 - Családi')
-    print('4 - Terepjáró')
-    print('5 - Városi SUV')
+    print('a - Kilépés')
+    print('b - Sport')
+    print('c - Családi, kisbusz')
+    print('d - Terepjáró')
+    print('e - Tuningok, extrák')
     return input('Kérem írja be a választott típus sorszámát: ')
 
-def loadCars():
+def loadSport(choises):
+    system('cls')
     file = open(filename, 'r', encoding='utf-8')
-    global cimsor
-    cimsor = file.readline()
-    for row in file:
-        splitted = row.strip().split(';')
-        cars[splitted[0]] = int(splitted[1])
+    fulldata = ''
+    
+    
+    if choises == '1':
+        data = szuperSport
+    elif choises == '2':
+        data = muscle
+    elif choises == '3':
+        data = sport
+    elif choises == '4':
+        data = hetszem
+    elif choises == '5':
+        data = nyolcszem
+    elif choises == '6':
+        data = kilencszem
+    elif choises == '7':
+        data = nehezterep
+    elif choises == '8':
+        data = varossuv
+    elif choises == '9':
+        data = legszurok
+    elif choises == '10':
+        data = ledek
+    elif choises == '11':
+        data = spoiler
+    elif choises == '12':
+        data = felnik
+    elif choises == '13':
+        data = turbok
+    
+        
+    for entry in data:
+        fulldata += 'Márka: '+entry['marka'].ljust(20)+' Lóerő: '+entry['HP'].ljust(10)+' Ár: '+entry['price']+'\n'
+   
     file.close()
+    return input(fulldata)
+
+def menuSport():
+    system('cls')
+    print('Milyen jellgű sportautót keres?')
+    print('0 - Vissza a menübe')
+    print('1 - Szupersport')
+    print('2 - Muscle')
+    print('3 - Sport')
+    return input('Választásának sorszáma: ')
+
+
+def menuCsaladi():
+    system('cls')
+    print('Hány személyes jármű érdekli?')
+    print('0 - Vissza a menübe')
+    print('4 - hét személyes')
+    print('5 - nyolc személyes')
+    print('6 - kilenc személyes')
+    return input('Kérem válassza ki hány személyes járműre van szüksége: ')
+
+def menuTerepjaro():
+    system('cls')
+    print('Milyen jellegű terepjáró érdekli?')
+    print('0 - Vissza a menübe')
+    print('7 - Nehéz terepre való terepjáró')
+    print('8 - Városi terepjáró')
+    return input('Választásának sorszáma: ')
+
+def menuExtrak():
+    system('cls')
+    print('Milyen extrákra lenne szüksége?')
+    print('0 - Vissza a menübe')
+    print('9 - Légszűrők')
+    print('10 - LED-ek')
+    print('11 - Spoiler tartozékok')
+    print('12 - Felnik')
+    print('13 - Turbók')
+    return input('Kérem írja be a tuning sorszámát, ami érdekli: ')
